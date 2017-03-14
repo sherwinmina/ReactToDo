af4c66ccfd23a5cdc4e5b288515d95e2717a3bb9
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-14 13:47:41 -0700

    refactored to work with redux

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,9 +1,11 @@
 var React = require('react');
+var {connect} = require('react-redux');
 var moment = require('moment');
+var actions = require('actions');
 
-var Todo = React.createClass({
+export var Todo = React.createClass({
   render: function () {
-    var {id, text, completed, createdAt, completedAt} = this.props;
+    var {id, text, completed, createdAt, completedAt, dispatch} = this.props;
     var todoClassName = completed ? 'todo todo-completed' : 'todo';
     var renderDate = () => {
       var message = 'Created ';
@@ -15,23 +17,22 @@ var Todo = React.createClass({
       }
 
       return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
-    }
+    };
 
     return (
       <div className={todoClassName} onClick={() => {
-          this.props.onToggle(id);}
-        }>
+          dispatch(actions.toggleTodo(id));
+        }}>
         <div>
-            <input type="checkbox" checked={completed}/>
+          <input type="checkbox" checked={completed}/>
         </div>
         <div>
           <p>{text}</p>
           <p className="todo__subtext">{renderDate()}</p>
         </div>
-
       </div>
     )
   }
 });
 
-module.exports = Todo;
+export default connect()(Todo);
--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -1,28 +1,36 @@
 var React = require('react');
-var ReactDOM = require('react-dom');
-var TestUtils = require('react-addons-test-utils');
-var expect = require('expect');
-var $ = require('jquery');
+var {connect} = require('react-redux');
+import Todo from 'Todo';
 
-var TodoList = require('TodoList');
-var Todo = require('Todo');
+export var TodoList = React.createClass({
+  render: function () {
+    var {todos} = this.props;
+    var renderTodos = () => {
+      if (todos.length === 0) {
+        return (
+          <p className="container__message">Nothing To Do</p>
+        );
+      }
 
-describe('TodoList', () => {
-  it('should exist', () => {
-    expect(TodoList).toExist();
-  });
+      return todos.map((todo) => {
+        return (
+          <Todo key={todo.id} {...todo}/>
+        );
+      });
+    };
 
-  it('should render one Todo component for each todo item', () => {
-    var todos = [{
-      id: 1,
-      text: 'Do something'
-    }, {
-      id: 2,
-      text: 'Check mail'
-    }];
-    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
-    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
-
-    expect(todosComponents.length).toBe(todos.length);
-  });
+    return (
+      <div>
+        {renderTodos()}
+      </div>
+    )
+  }
 });
+
+export default connect(
+  (state) => {
+    return {
+      todos: state.todos
+    };
+  }
+)(TodoList);
