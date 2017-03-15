b0725602f8774784ab51d324d5b634663c80896b
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-15 16:27:55 -0700

    fixing

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+// process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
   entry: [
@@ -21,7 +21,11 @@ module.exports = {
       compressor: {
         warnings: false
       }
-    })
+    }),
+    new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+    new webpack.EnvironmentPlugin(['NODE_ENV'])
   ],
   output: {
     path: __dirname,
