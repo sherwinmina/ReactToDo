cf00f0b8a5b7ef43383377d778c20d3b6a6b0181
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-15 15:44:53 -0700

    production config

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,8 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-    })
+    }),
+    new webpack.EnvironmentPlugin(['NODE_ENV'])
   ],
   output: {
     path: __dirname,
