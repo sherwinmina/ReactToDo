47ed3ba1374149a94e06a64eefb6319947d93fb2
Sherwin Mina <sherwinmina@yahoo.com>
2017-03-15 15:35:20 -0700

    fixing webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,9 @@ module.exports = {
       compressor: {
         warnings: false
       }
+    }),
+    new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     })
   ],
   output: {
