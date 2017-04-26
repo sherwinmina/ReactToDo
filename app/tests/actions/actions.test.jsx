4b1b2e91dd8f2e93b2242b58f363720914d803e3
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-26 10:36:14 -0700

    set up firebase auth test

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -42,22 +42,6 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
-  it('should create todo and dispatch ADD_TODO', (done) => {
-    const store = createMockStore({});
-    const todoText = 'My todo item';
-
-    store.dispatch(actions.startAddTodo(todoText)).then(() => {
-      const actions = store.getActions();
-      expect(actions[0]).toInclude({
-        type: 'ADD_TODO'
-      });
-      expect(actions[0].todo).toInclude({
-        text: todoText
-      });
-      done();
-    }).catch(done);
-  });
-
   it('should generate add todos action object', () => {
     var todos = [{
       id: '111',
@@ -163,6 +147,23 @@ describe('Actions', () => {
         done();
       }, done)
     });
+
+    it('should create todo and dispatch ADD_TODO', (done) => {
+      const store = createMockStore({});
+      const todoText = 'My todo item';
+
+      store.dispatch(actions.startAddTodo(todoText)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toInclude({
+          type: 'ADD_TODO'
+        });
+        expect(actions[0].todo).toInclude({
+          text: todoText
+        });
+        done();
+      }).catch(done);
+    });
+
   });
 });
 
