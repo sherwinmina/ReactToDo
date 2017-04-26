471a64c747c6e46b7f9333623c6e87f9a40cccc2
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-26 10:44:24 -0700

    refactored actions test for anonymous sign in

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -92,29 +92,34 @@ describe('Actions', () => {
 
   describe('Tests with firebase todos', () => {
     var testTodoRef;
-
+    var uid;
+    var todosRef;
+    
     beforeEach((done) => {
-      var todosRef = firebaseRef.child('todos');
+      firebase.auth().signInAnonymously().then((user) => {
+        uid = user.uid;
+        todosRef =firebaseRef.child(`users/${uid}/todos`);
 
-      todosRef.remove().then(() => {
-        testTodoRef = firebaseRef.child('todos').push();
+        return todosRef.remove();
+      }).then(() => {
+        testTodoRef = todosRef.push();
 
         return testTodoRef.set({
           text: 'Something to do',
           completed: false,
           createdAt: 23453453
-        })
+        });
       })
       .then(() => done())
       .catch(done);
     });
 
     afterEach((done) => {
-      testTodoRef.remove().then(() => done());
+      todosRef.remove().then(() => done());
     });
 
     it('should toggle todo and dispatch UPDATE_TODO action', (done) => {
-      const store = createMockStore({});
+      const store = createMockStore({auth: {uid}});
       const action = actions.startToggleTodo(testTodoRef.key, true);
 
       store.dispatch(action).then(() => {
@@ -134,7 +139,7 @@ describe('Actions', () => {
     });
 
     it('should populate todos and dispatch ADD_TODOS', (done) => {
-      const store = createMockStore({});
+      const store = createMockStore({auth: {uid}});
       const action = actions.startAddTodos();
 
       store.dispatch(action).then(() => {
@@ -149,7 +154,7 @@ describe('Actions', () => {
     });
 
     it('should create todo and dispatch ADD_TODO', (done) => {
-      const store = createMockStore({});
+      const store = createMockStore({auth: {uid}});
       const todoText = 'My todo item';
 
       store.dispatch(actions.startAddTodo(todoText)).then(() => {
